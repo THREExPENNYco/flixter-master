5dde18170d5982dda97343526e62fe7bea8015d7
Devoun <THREExPENNYco@gmail.com>
2019-09-14 22:07:46 +0000

    video added to lessons

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -22,7 +22,7 @@ class Instructor::CoursesController < ApplicationController
   private
 
   def require_authorized_for_current_course 
-    if current_user != current_user 
+    if current_course.user != current_user 
       render plain: "Unauthorized", status: :unauthorized 
     end 
   end 
