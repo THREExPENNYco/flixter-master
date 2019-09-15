a54a463a92dfdd784a842177a91d7dc2ebc99670
Devoun <THREExPENNYco@gmail.com>
2019-09-15 19:09:43 +0000

    adding enrollment authorization to lesson views

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,19 @@
 class LessonsController < ApplicationController
+ before_action :authenticate_user! 
+ before_action :enrolled_in_course_required, only: [:show]
+
   def show 
   end 
 
   private 
+
+  def enrolled_in_course_required 
+    if current_user.enrolled_in?(current_lesson.section.course)
+    else 
+      redirect_to course_path(current_lesson.section.course), alert: 'You Are Not Enrolled'
+    end 
+  end 
+
 
   helper_method :current_lesson 
   def current_lesson 
