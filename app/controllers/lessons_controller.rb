ea3520eeb394e59162d3d2c7ab00aaa02abee3db
Devoun <THREExPENNYco@gmail.com>
2019-09-20 07:58:47 +0000

    adjustments to css

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -2,21 +2,21 @@ class LessonsController < ApplicationController
  before_action :authenticate_user! 
  before_action :enrolled_in_course_required, only: [:show]
 
-  def show 
+ def show 
+ end 
+
+ private 
+
+ def enrolled_in_course_required 
+  if current_user.enrolled_in?(current_lesson.section.course)
+  else 
+    redirect_to course_path(current_lesson.section.course), alert: 'You Are Not Enrolled'
   end 
-
-  private 
-
-  def enrolled_in_course_required 
-    if current_user.enrolled_in?(current_lesson.section.course)
-    else 
-      redirect_to course_path(current_lesson.section.course), alert: 'You Are Not Enrolled'
-    end 
-  end 
+end 
 
 
-  helper_method :current_lesson 
-  def current_lesson 
-    @current_lesson ||= Lesson.find(params[:id])
-  end 
+helper_method :current_lesson 
+def current_lesson 
+  @current_lesson ||= Lesson.find(params[:id])
+end 
 end
